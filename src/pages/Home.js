c6ce6a09ddcc926b7ec19090cb21e55d89ebe1de
hashir4560 <haashirraja@gmail.com>
2023-09-20 20:51:56 +0500

    Home and About Done

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <div className="home">
+        <h2>Welcome to Your Ultimate Job Search Destination</h2>
+        <p>Welcome to our Job Search Website, your ultimate destination for finding the perfect job opportunity that matches your skills and aspirations. We understand that your career is important, and we're here to make your job search journey as seamless and successful as possible.</p>
+        <p>Our platform offers a wide range of job listings from top companies across various industries. Whether you're just starting your career, looking for a career change, or aiming to advance in your current field, you'll find valuable resources and job listings tailored to your needs.</p>
+        <p>Start exploring our website today to discover exciting career options, expert advice, and tools that will help you take the next step in your professional journey. Your dream job is just a click away!</p>
+      </div>
+    );
+  }
+  
