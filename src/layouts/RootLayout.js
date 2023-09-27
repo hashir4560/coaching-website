295644f3566b8c9fefef0f37c3f2e9ed246cdc25
hashir4560 <haashirraja@gmail.com>
2023-09-27 20:25:54 +0500

    first commit

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -6,7 +6,11 @@ export default function RootLayout() {
     <div className="root-layout">
       <header>
         <nav>
-          <h1>Job Searcher</h1>
+          <h1>
+            <span style={{ color: "#FFA500" }}>Quantum</span>{" "}
+            <span style={{ color: "#ffff" }}>Learning</span>{" "}
+            <span style={{ color: "#FFA500" }}>Academy</span>
+          </h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
           <NavLink to="help">Help</NavLink>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,43 @@
+import React, { useState, useEffect } from "react";
 export default function Home() {
-    return (
-      <div className="home">
-        <h2>Welcome to Your Ultimate Job Search Destination</h2>
-        <p>Welcome to our Job Search Website, your ultimate destination for finding the perfect job opportunity that matches your skills and aspirations. We understand that your career is important, and we're here to make your job search journey as seamless and successful as possible.</p>
-        <p>Our platform offers a wide range of job listings from top companies across various industries. Whether you're just starting your career, looking for a career change, or aiming to advance in your current field, you'll find valuable resources and job listings tailored to your needs.</p>
-        <p>Start exploring our website today to discover exciting career options, expert advice, and tools that will help you take the next step in your professional journey. Your dream job is just a click away!</p>
-      </div>
-    );
-  }
-  
+  const [text, setText] = useState("");
+  const welcomeText = "Welcome to Quantum Learning Academy!";
+  useEffect(() => {
+    let currentIndex = 0;
+    const typingInterval = setInterval(() => {
+      if (currentIndex <= welcomeText.length) {
+        setText(welcomeText.substring(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(typingInterval);
+        // After typing is complete, set the text without the cursor
+        setText(welcomeText);
+      }
+    }, 100); // Adjust the typing speed by changing the interval duration
+  }, []);
+
+  return (
+    <div className="home">
+      <h2>{text}</h2>
+      <p>
+        Welcome to Quantum Learning Academy, your ultimate destination for
+        quality education. We offer a diverse range of courses tailored to meet
+        the educational needs of students from Class 1 to Class VIII,
+        Matriculation, Intermediate, O Levels, and more.
+      </p>
+      <p>
+        At Quantum Learning Academy, we provide comprehensive Aptitude
+        Preparation, English Courses, and Computer Courses. Whether you're
+        looking to strengthen your academic foundation, enhance your
+        professional skills, or prepare for upcoming exams, we have the
+        resources and expertise to support your learning journey.
+      </p>
+      <p>
+        Explore our website to discover our exciting courses and expert
+        instructors. We are dedicated to helping you achieve your educational
+        goals and excel in your academic and professional endeavors. Your path
+        to success starts here!
+      </p>
+    </div>
+  );
+}
